b5e4b8a8659fddb76ed9dd8973527d94b4c270fc
Abhishek <annu100292@gmail.com>
2018-03-25 16:52:28 +0530

    POST /todos test cases & GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,9 +21,18 @@ app.post('/todos', (req, res)=>{
     });
 })
 
+app.get('/todos', (req, res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    }).catch((err)=>{
+        res.status(400).send(err);
+    })
+})
 
 
 
 app.listen(3000, ()=>{
    console.log('Server is running at port 3000');
 });
+
+module.exports = {app};
