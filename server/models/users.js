753313de11783b0af5013af7055bd03b770d2926
Abhishek <annu100292@gmail.com>
2018-04-19 01:28:27 +0530

    Route NewUser and User Auth

--- server/models/users.js
+++ server/models/users.js
@@ -1,11 +1,58 @@
 var mongoose = require('mongoose');
+var validator = require('validator');
+var jwt = require('jsonwebtoken');
+var _ = require('lodash');
 
-var User = mongoose.model('User', {
-    Email : {
+var UserSchema = new mongoose.Schema({
+    email : {
         type : String,
         required : true,
-        trim : true
-    }
+        trim : true,
+        minlength : 1,
+        unique : true,
+        validate : {
+            validator : validator.isEmail,
+            message: '{VALUE} is not a valid email id!'
+        }
+    },
+    password : {
+        type : String,
+        required : true,
+        minlength : 6
+    },
+    tokens : [{
+        access : {
+            type : String,
+            required : true
+        },
+        token : {
+            type : String,
+            required : true
+        }
+    }]
 });
+
+
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, "abc123").toString();
+
+    user.tokens.push({access, token});
+
+    return user.save().then( ()=> {
+        return token;
+    })
+};
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todos.js');
@@ -47,6 +49,7 @@ app.get('/todos/:id', (req, res)=>{
 
 app.delete('/todos/:id', (req, res)=>{
     var id = req.params.id;
+    console.log(req.body);
     if (!ObjectID.isValid(id)){
         return res.status(404).send('Invalid Object ID')
     }
@@ -59,6 +62,50 @@ app.delete('/todos/:id', (req, res)=>{
         res.status(400).send(err);
     })
 });
+
+app.patch('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Invalid Object ID')
+    }
+    var body = _.pick(req.body, ["text", "completed"]);
+
+    console.log(body.completed);
+    if (body.completed == true){
+        body.completedAt = new Date().getTime();
+    } else {
+
+        body.completedAt = null;
+        body.completed = false;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set : body}, {new : true}).then((todo)=>{
+        if (!todo){
+    return res.status(404).send("Todo not found");
+    }
+    res.send({todo});
+    }).catch((err)=>{
+        res.status(400).send(err);
+    });
+
+});
+
+// New User Creation
+app.post('/users', (req, res)=> {
+    var body = _.pick(req.body, ["email", "password"]);
+    var user = new User(body);
+
+    user.save().then(()=>{
+        // res.send(user);
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth', token).send(user);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
+});
+
+
 
 app.listen(3000, ()=>{
    console.log('Server is running at port 3000');
